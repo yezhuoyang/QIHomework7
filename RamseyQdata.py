2570bcfe48de887cdc276141edfc222c46196b8a
Zhuoyang ye <737475845@qq.com>
2024-03-10 16:03:39 -0800

    Add ramsey spectrum

--- RamseyQdata.py
+++ RamseyQdata.py
@@ -1,13 +1,21 @@
+import numpy as np
+
+
+def ramsey(Omega, Omegap, delta, tw, tp):
+    prob1 = Omegap * np.cos(delta * tw / 2) * np.sin(Omegap * tp)
+    prob2 = 2 * delta * np.sin(delta * tw / 2) * np.sin(Omegap * tp / 2) ** 2
+    return 1 - Omega ** 2 / Omegap ** 4 * (prob1 - prob2) ** 2
+
+
+def plot_tw():
+    N = 1101001000
+    Omega = 2 * np.pi
+    Omegap = np.pi / 4
+    twlist=0
+    pass
 
 
 
 
-
-
-
-
-
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     pass
